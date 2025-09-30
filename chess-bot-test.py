a6f92606f46e1d2d1747e7868d100b3d152bf795
marybellam <123906460+marybellam@users.noreply.github.com>
2025-09-30 17:14:35 -0400

    Update chess-bot-test.py

--- chess-bot-test.py
+++ chess-bot-test.py
@@ -1,26 +1,6 @@
 from datetime import datetime
 import chess
-import random
 
-def botPlay(board):
-    board.legal_moves.count()
-    legal_moves = list(board.legal_moves)
-    num = random.randint(0, board.legal_moves.count() - 1)
-    move = legal_moves[num]
-    print("Bot plays: " + move.uci())
-    board.push(move)
-    print(board)
-    
-def humanPlay(board):
-    legal_moves = list(board.legal_moves)
-    move = input("Your move: ")
-    while move not in legal_moves:
-        print("Enter a valid move.")
-        move = input("Your move: ")
-    print("Human plays: " + move.uci())
-    board.push(move)
-    print(board)
-    
 # print date & time
 now = datetime.now()
 time = now.strftime("%H:%M:%S")
@@ -45,30 +25,28 @@ while True:
         break
     else:
         try:
-            board = chess.Board(start_pos) #use set_fen()?
+            board = chess.Board(start_pos)
             break
         except ValueError:
             print("Enter a valid FEN position.")
 
 # playing
-print(board)
 while not board.is_game_over():
-    if (board.turn == True): #whites turn
+    print(board)
+    if (board.turn == True):
         if bot_play == "w":
             print("Bot turn")
-            botPlay(board)
         else:
-            print("Player turn")
-            humanPlay(board)
+            print("Your turn")
     else:
         if bot_play == "b":
             print("Bot turn")
-            botPlay(board)
         else:
             print("Player turn")
-            humanPlay(board)
+    break
 print("Game over: ", board.result()) # result
 
-
+def wait():
+    if
 def main():
     print("This is a chess bot.")
