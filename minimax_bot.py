d47fc01891ef270866d13d7e08081847d39a6571
marybellam <mmartinez31@smith.edu>
2025-12-14 22:49:56 -0500

    updates

--- minimax_bot.py
+++ minimax_bot.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 import chess
-import random
 import math
 
 # Chess Bot class
@@ -14,9 +13,9 @@ class ChessBot:
     chess.KING: 0
     }
 
-    def __init__(self, bot_color=None):
+    def __init__(self):
         self.board = chess.Board()
-        self.bot_color = bot_color
+        self.bot_color = None
         self.player_color = None
 
     # Method to print current date & time
@@ -26,8 +25,8 @@ class ChessBot:
         date = now.strftime("%d/%m/%y")
         print("Time: " + date + time)
 
-    # Method to choose computer player
     def computer_player(self):
+        """Choosing Computer Player"""
         while True:
             bot_play = input("Computer Player? (w=white/b=black): ").lower()
             if bot_play == "w" or bot_play == "b":
@@ -41,8 +40,8 @@ class ChessBot:
             else:
                 print("Enter a valid character.")
 
-    # Method to choose start position
     def board_start_pos(self):
+        """Sets start position of game"""
         while True:
             start_pos = input("Starting FEN position? (ENTER for standard starting position): ")
             if start_pos == "":
@@ -55,20 +54,9 @@ class ChessBot:
                     break
                 except ValueError:
                     print("Enter a valid FEN position.")
-
-    # Bot's turn
-    def bot_play(self):
-        legal_moves = list(self.board.legal_moves)
-        captures = [move for move in legal_moves if self.board.is_capture(move)]
-        if captures:
-            move = random.choice(captures)
-        else:
-            move = random.choice(legal_moves)
-        print(move.uci())
-        self.board.push(move)
-        
-    # Player's turn
+                    
     def human_play(self):
+        """Gets Players Move"""
         legal_moves = list(self.board.legal_moves)
         move = input("Your move: ")
         while move not in [m.uci() for m in legal_moves]:
@@ -77,46 +65,11 @@ class ChessBot:
         self.board.push_uci(move)
         print(self.board)
 
-    # Milestone
-    def shallow_minmax(self):
-        best_score = float('-inf')
-        best_move = None
-            
-        for white_move in list(self.board.legal_moves): #for each white move
-            if self.board.is_capture(white_move):
-                white_capture = self.board.piece_at(white_move.to_square) #gets the piece being captured
-            else:
-                white_capture = None
-            if white_capture != None:
-                value_white = self.piece_values[white_capture.piece_type] #value of piece being captured
-            else:
-                value_white = 0
-            self.board.push(white_move)
-
-            worst_black_score = float('inf')
-            for black_move in list(self.board.legal_moves):
-                black_capture = self.board.piece_at(black_move.to_square)
-                if black_capture != None:
-                    value_black = self.piece_values[black_capture.piece_type]
-                else:
-                    value_black = 0
-                score = value_white - value_black
-                if score < worst_black_score: #gets lowest score/worst for white score
-                    worst_black_score = score
-            if(worst_black_score > best_score):
-                best_score = worst_black_score
-                best_move = white_move
-            self.board.pop()
-        if best_move:
-            print(best_move.uci())
-            self.board.push(best_move)
-        else:
-            print("ERROR: no move made")
-
-    def evalBoard(self, board): 
+    def evalBoard(self): 
+        """Get board score from white's perspective"""
         score = 0
         for square in chess.SQUARES:
-            piece = self.board.piece_at(square)  # Keep using self.board
+            piece = self.board.piece_at(square) 
             if piece is not None:
                 val = self.piece_values[piece.piece_type]
                 if piece.color == chess.WHITE:
@@ -125,69 +78,73 @@ class ChessBot:
                     score -= val
         return score
     
-    def Min(self,board,depth):
+    def Min(self,depth:int):
+        """ Gets min of opponent's turn"""
+        if self.board.is_checkmate():
+            return math.inf,None
         if depth == 0 or self.board.is_game_over():
-            eval_score = self.evalBoard(board)
-            return (eval_score if self.bot_color == "w" else -eval_score), None
-        best_score = float('inf')
+            eval_score = self.evalBoard()
+            if self.bot_color == "b":
+                eval_score = -eval_score
+            return eval_score, None
+            #return (eval_score if self.bot_color == "w" else -eval_score), None
+        best_score:float|int = math.inf
         best_move = None
-        
-        for move in self.board.legal_moves: #for each white move
+        for move in self.board.legal_moves: 
             self.board.push(move)
-            score,_ = self.Max(board, depth-1)
+            score,_ = self.Max(depth-1)
+            self.board.pop()
             if(score < best_score):
                 best_score = score
                 best_move = move
-            self.board.pop()
         return best_score, best_move
     
-    def Max(self,board,depth):
+    def Max(self,depth:int):
+        """Gets max of plater/bots turn"""
+        if self.board.is_checkmate():
+            return -math.inf,None #lost
         if depth == 0 or self.board.is_game_over():
-            eval_score = self.evalBoard(board)
-            return (eval_score if self.bot_color == "w" else -eval_score), None
-        best_score = float('-inf')
+            eval_score = self.evalBoard()
+            if self.bot_color == "b":
+                eval_score = -eval_score
+            return eval_score, None
+            #return (eval_score if self.bot_color == "w" else -eval_score), None
+        best_score = -math.inf
         best_move = None
         
-        for move in self.board.legal_moves: #for each white move
+        for move in self.board.legal_moves: 
             self.board.push(move)
-            score,_ = self.Min(board, depth-1)
+            score,_ = self.Min(depth-1)
+            self.board.pop()
             if(score > best_score):
                 best_score = score
                 best_move = move
-            self.board.pop()
         return best_score, best_move
     
-    # Gameplay
     def game(self):
+        """Gamplay, printing the board and checking for game over """
         while not self.board.is_game_over():
             print(self.board)
-            if (self.board.turn == True):
-                if self.bot_color == "w":
-                    print("Bot (as white): ")
-                    score,move = self.Max(self.board,2)
-                    if move == None:
-                        print("ERROR")
-                    else:
-                        print(move.uci())
-                        self.board.push(move)
-                else:        
-                    self.human_play()
-                print("New FEN position: " + self.board.fen())
-            else:
+            print()
+            current_turn_w =self.board.turn
+            bot = (current_turn_w and self.bot_color == "w") or (not current_turn_w and self.bot_color == "b")
+            if bot == True:
+                color_name = "white"
                 if self.bot_color == "b":
-                    print("Bot (as black): ")
-                    score,move = self.Max(self.board,2)
-                    if move == None:
-                        print("ERROR")
-                    else:
-                        print(move.uci())
-                        self.board.push(move)
+                    color_name = "black"
+                print("Bot (as",color_name,"): ") 
+                _,move = self.Max(2)
+                if move == None:
+                    print("ERROR")
                 else:
-                    self.human_play()
-                print("New FEN position: " + self.board.fen())
+                    print(move.uci())
+                    self.board.push(move)
+            else:
+                self.human_play()
+            print("New FEN position: " + self.board.fen())
             if self.board.is_game_over():
                 break
-
+        print(self.board)
         print("Game over: ", self.board.result()) # result
  
     # Set up game
